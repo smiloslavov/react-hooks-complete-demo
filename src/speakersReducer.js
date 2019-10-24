ef0c5fa6152d6533dea6bd8c4cf32a8237eecf09
Svetlomir Miloslavov <smiloslavov@yahoo.com>
2019-10-24 19:25:10 +0300

    useContext and useReducer demo

---
+++ src/speakersReducer.js
@@ -0,0 +1,30 @@
+function speakersReducer(state, action) {
+    
+    function updateFavorite(favoriteValue) {
+      return state.map( (item, index) => {
+        if (item.id === action.sessionId) {
+          item.favorite = favoriteValue;
+        }
+
+        return item;
+      });
+    }
+    
+    switch (action.type) {
+      case "setSpeakerList": {
+        return action.data;
+      }
+      case "favorite": {
+        return updateFavorite(true);
+      }
+      case "unfavorite": {
+        return updateFavorite(false);
+      }
+      default:
+        return state;
+    }
+
+    return action;
+}
+
+export default speakersReducer;
